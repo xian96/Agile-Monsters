a370f424c0753a5a750ba0b535c3367277bc5ae1
xian96 <42896816+xian96@users.noreply.github.com>
2020-05-17 14:02:21 -0400

    can not connect  atlas

--- backend/config/mongoConnection.js
+++ backend/config/mongoConnection.js
@@ -6,7 +6,7 @@ let _db = undefined;
 module.exports = async () => {
    if (!_connection) {
       _connection = await MongoClient.connect(`mongodb+srv://jason:${process.env.MONGO_ATLAS_API_KEY}@cluster0-jsdmw.mongodb.net/test?retryWrites=true&w=majority`, { useUnifiedTopology: true });
-      _db = await _connection.db("Agile-Monsters-CS554-Final");
+      _db = await _connection.db("test");
    }
 
    return _db;
