6758098844aaecf4a475882e8af4bba82699e1bb
xian96 <42896816+xian96@users.noreply.github.com>
2021-01-05 00:53:47 -0500

    login auth not showed when authUser middleware check

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 const authUser = async (req, res, next) => {
    console.log('----- authUser -------')
-   console.log(req.session);
    if (!req.session.userId && !req.session.auth) {
       console.log('-----not log in redirect to logout -------')
+      console.log(req.session);
       return res.redirect('/users/logout');
       // return res.redirect('/logout');
    }
