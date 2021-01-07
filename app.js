c85b323638b56af1b01840d9ec71949de5cb634b
xian96 <42896816+xian96@users.noreply.github.com>
2021-01-06 23:53:47 -0500

    cookie setting

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
        maxAge: 60 * 1000 * 30,
-       httpOnly: true,
-       secure: true,
-       sameSite: "none",
+       // httpOnly: true,
+       // secure: true,
+       // sameSite: "none",
     }
   })
 )
